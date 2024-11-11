b4144246c11d0efa97621f2babe60a071a4cb299
ksenera <ksenera7130@conestogac.on.ca>
2024-11-10 22:14:25 -0500

    added a param should be numbers test

--- src/calculator/add.test.ts
+++ src/calculator/add.test.ts
@@ -5,7 +5,7 @@ describe('addition', () => {
 
   test('when the two inputs are added, the expected value is returned', () => {
    // start
-   const additionService: AdditionService  = new SimpleAdditionService()
+   const additionService: AdditionService  = new SimpleAdditionService();
    // const additionService: AdditionService  = new ComplexAdditionService()
    const var1 = 100;
    const var2 = 1000;
@@ -17,7 +17,7 @@ describe('addition', () => {
   });
   
   // Test for unexpected value being returned 
-  test('there should be an error if the calculated value is incorrect'), () => {
+  test('there should be an error if the calculated value is incorrect', () => {
    const additionService: AdditionService = new SimpleAdditionService();
    const var1 = 100;
    const var2 = 200;
@@ -27,7 +27,13 @@ describe('addition', () => {
    // 500 is incorrect to see if this test passes 
    expect(result).toBe(500);
   });
-  test.todo('the param should be numbers');
+
+  // Test for valid input types 
+  test('the param should be numbers', () => {
+   const additionService: AdditionService = new SimpleAdditionService();
+   expect(() => additionService.add(["100" as any, 200])).toThrowError
+   ('the param should be numbers');
+  });
   test.todo('the param should not be string');
 });
 
